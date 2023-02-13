c4756cd9a9ec033a59b8785c6f3a5ecaa6bb3bcf
Alex <alexstarochkin@gmail.com>
2023-02-13 19:17:25 +0700

    update 13.02

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from vkbottle import Bot
 from handlers import labelers
+from config import TOKEN
 
-TOKEN = "vk1.a.wPRq7JIWA608ZdNPzMwWSQNBiKsVu4ld7OJiXXQ0KLb1lMgZU43SZ2GJxG15H__NWIQumL49hCUnlgTnmcztyNu2r2sA5DqTT6HwoqD1nYJcVxHxREU5JpjyEK6I1zTvsPUfLZ2yvUSyO66BS1cVVkTWcGuKc1Tp2bZlLhSU3zunSEU6RD4c8merDFqZr9caZ69ZE_u44bWu_ZrnvRwsaw"
 
 bot = Bot(TOKEN)
 
